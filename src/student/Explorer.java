e2d934f319f1864d28a190758deb1a68e7baa37b
Jade <jadedickinson@googlemail.com>
2016-04-17 23:15:03 +0100

    1050 works

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -130,7 +130,7 @@ public class Explorer {
             Edge checkLength = checkWayOut.get(i).getEdge(checkWayOut.get(i+1));
             sumOfCosts = sumOfCosts + checkLength.length;
         }
-        if (state.getTimeRemaining() - 1100 < sumOfCosts) {
+        if (state.getTimeRemaining() - 1050 < sumOfCosts) {
             if (state.getCurrentNode().equals(exitNode)) {
                 return;
             }
