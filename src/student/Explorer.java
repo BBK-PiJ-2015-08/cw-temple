e9cf30e547196dddeeab580ccff758a765f2dd50
Jade <jadedickinson@googlemail.com>
2016-04-17 17:46:24 +0100

    Fixed for -s 4210940078797672240. Comparison between getTimeRemaining and length of pathToExit can be length of exit

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -140,7 +140,7 @@ public class Explorer {
             sumOfCosts = sumOfCosts + checkLength.length;
         }
 
-        if (state.getTimeRemaining() - 807 < sumOfCosts) {
+        if (state.getTimeRemaining() - 1249 < sumOfCosts) {
             if (state.getCurrentNode().equals(exitNode)) {
                 return;
             }
