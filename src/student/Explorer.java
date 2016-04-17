62231805899aea0ef8143a0d23ec446d38ceb4f7
Jade <jadedickinson@googlemail.com>
2016-04-17 23:45:25 +0100

    Tiny further adjustment to handle -6469114583799938252

--- src/student/Explorer.java
+++ src/student/Explorer.java
@@ -145,7 +145,7 @@ public class Explorer {
             Edge checkLength = checkWayOut.get(i).getEdge(checkWayOut.get(i+1));
             sumOfCosts = sumOfCosts + checkLength.length;
         }
-        if (state.getTimeRemaining() - 1213 < sumOfCosts) {
+        if (state.getTimeRemaining() - 1292 < sumOfCosts) {
             if (state.getCurrentNode().equals(exitNode)) {
                 return;
             }
